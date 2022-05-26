7da5372f370d4eeaf03f9a572b6dc67d721d0fef
ceram <ceramjt@153.com>
2022-05-26 21:57:50 +0800

    TextureView

---
+++ app/src/main/java/com/ceram/camerav2api/AutoTextureView.java
@@ -0,0 +1,14 @@
+package com.ceram.camerav2api;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AutoTextureView extends TextureView {
+    public AutoTextureView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
