849c958d73a6fad4ece6b7488793139c1fae7fee
ZJTceram <ceramZJT@outlook.com>
2022-05-31 22:00:36 +0800

    {[CAMERA.APP.OTHER]【新架构】自定义TextureView与Camera的权限书写}

--- app/src/main/java/com/ceram/camerav2api/AutoTextureView.java
+++ app/src/main/java/com/ceram/camerav2api/AutoTextureView.java
@@ -8,7 +8,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class AutoTextureView extends TextureView {
+    public int mRatioWidth = 0;
+    public int mRatioHeight = 0;
+
     public AutoTextureView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public AutoTextureView(BaseActivity context, Object attrs) {
+        super(context, (AttributeSet) attrs);
+    }
+
+    void setAspectRation(int width, int height) {
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        }else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            }else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/ceram/camerav2api/BaseActivity.java
@@ -0,0 +1,135 @@
+package com.ceram.camerav2api;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class BaseActivity extends AppCompatActivity {
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 180);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 360);
+    }
+
+    private FrameLayout mFrameLayout;
+    private AutoTextureView mAutoTextureView;
+    private CameraDevice mCameraDevice;
+    private Size mSize;
+    private CaptureRequest.Builder mPreviewRequestBuilder;
+    private CaptureRequest mPreviewRequest;
+    private CameraCaptureSession mCameraCaptureSession;
+    private ImageReader mImageReader;
+
+    private String mbCameraID = "0";
+
+    private final TextureView.SurfaceTextureListener mSurfaceTextureListener
+            = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFrameLayout = findViewById(R.id.FrameLayout);
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, 0X123);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0x123 && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mAutoTextureView =new AutoTextureView(BaseActivity.this, null);
+            mAutoTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+            mFrameLayout.addView(mAutoTextureView);
+            findViewById(R.id.Capture).setOnClickListener(View -> CaptureStillPicture());
+        }
+    }
+
+    private void CaptureStillPicture(){
+        try {
+            if (null == mCameraDevice){
+                return;
+            }
+
+            CaptureRequest.Builder mCaptureRequestBuilder =mCameraDevice
+                    .createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            mCaptureRequestBuilder.addTarget(mImageReader.getSurface());
+            mCaptureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            mCaptureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                    CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+
+            mCaptureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION,
+                    ORIENTATIONS.get(rotation));
+
+            mCameraCaptureSession.stopRepeating();
+            mCameraCaptureSession.capture(mCaptureRequestBuilder.build(),
+                    new CameraCaptureSession.CaptureCallback() {
+                        @Override
+                        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                            super.onCaptureCompleted(session, request, result);
+                            try {
+                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
+                                        CameraMetadata.CONTROL_AF_TRIGGER_CANCEL);
+                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                                        CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                                mCameraCaptureSession.setRepeatingRequest(mPreviewRequest, null, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
